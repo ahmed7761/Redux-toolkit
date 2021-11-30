c95ded2998f18a3632b80188f6e675c5c18e156b
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-11-30 18:56:45 +0600

    login with useSelector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useState} from "react";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {loginAction} from "./redux/action"
 
 function App() {
@@ -8,12 +8,28 @@ function App() {
   const [phone, setPhone] = useState("");
 
   const dispatch = useDispatch();
+  const auth = useSelector(state => state.auth);
+
+  console.log(auth);
 
   return (
     <div className="App">
-      <h3>Login</h3>
-      <input name="phone" type="text" value={phone} onChange={e => setPhone(e.target.value)} placeholder="Enter phone" />
-      <button onClick={() => dispatch(loginAction(phone))}>Login</button>
+      {auth.isAuth ? (
+          <div>
+            <p>Welcome</p>
+            <p>Phone: {auth.phone}</p>
+          </div>
+      )
+
+      : (
+          <div>
+            <h3>Login</h3>
+            <input name="phone" type="text" value={phone} onChange={e => setPhone(e.target.value)} placeholder="Enter phone" />
+            <button onClick={() => dispatch(loginAction(phone))}>Login</button>
+          </div>
+      )
+
+      }
     </div>
   );
 }
